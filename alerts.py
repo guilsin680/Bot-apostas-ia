8002e780f9a6adebf2633c17cb96d65ec227eb7c
guilsin680 <guilherme123hj@outlook.com>
2025-04-10 23:21:47 -0300

    alerts

---
+++ alerts.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+def send_telegram_message(message):
+    token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+    if not token or not chat_id:
+        print("Token ou Chat ID do Telegram não estão definidos.")
+        return
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    data = {"chat_id": chat_id, "text": message}
+    response = requests.post(url, data=data)
+    if response.status_code != 200:
+        print("Erro ao enviar mensagem para o Telegram:", response.text)
